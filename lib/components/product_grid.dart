9af0b7f9fcbeb6e28e0e38829416c2146ee6d249
Aimberê Galdino <biroska@gmail.com>
2021-11-13 14:36:03 -0300

    ChangeNotifierProvider.value

--- lib/components/product_grid.dart
+++ lib/components/product_grid.dart
@@ -13,8 +13,11 @@ class ProductGrid extends StatelessWidget {
     return GridView.builder(
         // item count evita que sejam iterados itens que nao existem dando erro na tela
         itemCount: loadedProducts.length,
-        itemBuilder: (ctx, index) => ChangeNotifierProvider(
-          create: (_) => loadedProducts[ index ],
+
+        // Nesse ponto usa-se ChangeNotifierProvider.value porque o produto/notifier jah existe loadedProducts[ index ]
+        // Se fosse criado um novo notifier poderiamos ter problemas
+        itemBuilder: (ctx, index) => ChangeNotifierProvider.value(
+          value: loadedProducts[ index ],
           child: ProductItem(),
         ),
         //Sliver uma area com scrol na tela
