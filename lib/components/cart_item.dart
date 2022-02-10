13899896c63e35260fb966882cb27cc47834e86e
Aimbere Galdino <biroska@gmail.com>
2022-02-09 22:52:41 -0300

    Implementando Dismissible para remover produto do carrinho

--- lib/components/cart_item.dart
+++ lib/components/cart_item.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:shop/models/cart.dart';
 import 'package:shop/models/cart_item.dart';
 
 class CartItemWidget extends StatelessWidget {
@@ -8,23 +10,39 @@ class CartItemWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Card(
-      margin: const EdgeInsets.symmetric(horizontal: 15, vertical: 4),
-      child: Padding(
-        padding: const EdgeInsets.all(8),
-        child: ListTile(
-          leading: CircleAvatar(
-            backgroundColor: Theme.of(context).primaryColor,
-            child: Padding(
-              padding: const EdgeInsets.all( 5 ),
-              child: FittedBox(
-                child: Text('${cartItem.price}', style: TextStyle( color: Theme.of(context).primaryTextTheme.headline6?.color ), ),
+    // Dismissible --> Efeito de arrastar o card, para fazer uma acao
+    return Dismissible(
+      key: ValueKey( cartItem.id,),
+      direction: DismissDirection.endToStart,
+      background: Container(
+        color: Theme.of(context).errorColor,
+        child: const Icon( Icons.delete, color: Colors.white, size: 40, ),
+        alignment: Alignment.centerRight,
+        padding: const EdgeInsets.only(right: 20),
+        margin: const EdgeInsets.symmetric(horizontal: 15, vertical: 4),
+      ),
+      onDismissed: (_){
+        // Se o item nao for removido, por nao encontrar o ID, ocorre exception
+        Provider.of<Cart>(context, listen: false).removeItem( cartItem.productId );
+      },
+      child: Card(
+        margin: const EdgeInsets.symmetric(horizontal: 15, vertical: 4),
+        child: Padding(
+          padding: const EdgeInsets.all(8),
+          child: ListTile(
+            leading: CircleAvatar(
+              backgroundColor: Theme.of(context).primaryColor,
+              child: Padding(
+                padding: const EdgeInsets.all( 5 ),
+                child: FittedBox(
+                  child: Text('${cartItem.price}', style: TextStyle( color: Theme.of(context).primaryTextTheme.headline6?.color ), ),
+                ),
               ),
             ),
+            title: Text( cartItem.name ),
+            subtitle: Text( 'Total R\$ ${cartItem.price * cartItem.quantity}' ),
+            trailing: Text( '${cartItem.quantity}X' ),
           ),
-          title: Text( cartItem.name ),
-          subtitle: Text( 'Total R\$ ${cartItem.price * cartItem.quantity}' ),
-          trailing: Text( '${cartItem.quantity}X' ),
         ),
       ),
     );
