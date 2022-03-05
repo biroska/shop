98526042d95ac56c84362e1c1b52254977013bc5
Aimbere Galdino <biroska@gmail.com>
2022-03-05 14:01:53 -0300

    Tela detalhes do produto

--- lib/screens/product_detail_screen.dart
+++ lib/screens/product_detail_screen.dart
@@ -3,8 +3,7 @@ import 'package:shop/models/product.dart';
 
 class ProductDetailScreen extends StatelessWidget {
 
-  const ProductDetailScreen({Key? key})
-      : super(key: key);
+  const ProductDetailScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -14,6 +13,26 @@ class ProductDetailScreen extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         title: Text( product.name ),
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Container(
+              height: 300,
+              width: double.infinity,
+              child: Image.network( product.imageUrl, fit: BoxFit.cover, ),
+            ),
+            const SizedBox( height: 10, ),
+            Text( 'R\$ ${product.price}',
+            style: const TextStyle(color: Colors.grey, fontSize: 20) ,),
+            const SizedBox( height: 10, ),
+            Container(
+              padding: const EdgeInsets.symmetric(horizontal: 10),
+              width: double.infinity,
+              child: Text( product.description, textAlign: TextAlign.center, ),
+            )
+          ],
+        ),
       ),
     );
   }
