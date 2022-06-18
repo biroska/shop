263174cf31cc4db026e143acb2407b65adc35aad
Aimbere Galdino <biroska@gmail.com>
2022-06-18 14:19:19 -0300

    Tratando erros no consumo da API

--- lib/screens/product_form_screen.dart
+++ lib/screens/product_form_screen.dart
@@ -82,14 +82,21 @@ class _ProductFormScreenState extends State<ProductFormScreen> {
       return;
     }
 
-    setState(() => _isLoading = true );
+    setState(() => _isLoading = true);
 
     _formKey.currentState?.save();
 
     Provider.of<ProductList>(context, listen: false)
-        .saveProduct(_formData).then((value) {
-          setState(() => _isLoading = false );
-          Navigator.of(context).pop();
+        .saveProduct(_formData)
+        .catchError((error) {
+      return showDialog<void>(context: context, builder: (ctx) => AlertDialog(
+        title: Text("Ocorreu um erro!"),
+        content: Text(error.toString()),
+        actions: [TextButton(onPressed: ()=> Navigator.of(context).pop(), child: Text('OK'))],
+      ) );
+    }).then((value) {
+      setState(() => _isLoading = false);
+      Navigator.of(context).pop();
     });
   }
 
@@ -102,115 +109,123 @@ class _ProductFormScreenState extends State<ProductFormScreen> {
           IconButton(onPressed: _submitForm, icon: const Icon(Icons.save)),
         ],
       ),
-      body: _isLoading ? Center( child: CircularProgressIndicator() ,)  :  Padding(
-        padding: const EdgeInsets.all(15),
-        child: Form(
-            key: _formKey,
-            child: ListView(
-              children: [
-                TextFormField(
-                  initialValue: _formData['name']?.toString(),
-                  decoration: const InputDecoration(
-                    labelText: 'Nome',
-                  ),
-                  textInputAction: TextInputAction.next,
-                  onFieldSubmitted: (_) {
-                    FocusScope.of(context).requestFocus(_priceFocus);
-                  },
-                  onSaved: (name) => _formData['name'] = name ?? '',
-                  validator: (_name) {
-                    final name = _name ?? '';
-                    if (name.trim().isEmpty) {
-                      return 'Nome é obrigatório';
-                    }
-                    if (name.trim().length < 3) {
-                      return 'Nome precisa no mínimo 3 letras';
-                    }
-                    return null;
-                  },
-                ),
-                TextFormField(
-                  initialValue: _formData['price']?.toString(),
-                  decoration: const InputDecoration(
-                    labelText: 'Preço',
-                  ),
-                  focusNode: _priceFocus,
-                  keyboardType:
-                      const TextInputType.numberWithOptions(decimal: true),
-                  textInputAction: TextInputAction.next,
-                  onSaved: (price) =>
-                      _formData['price'] = double.parse(price ?? '0'),
-                  validator: (_price) {
-                    final priceString = _price ?? '';
-                    final price = double.tryParse(priceString) ?? -1;
-                    if (price <= 0) {
-                      return 'Informe um preço válido';
-                    }
-                    return null;
-                  },
-                  onFieldSubmitted: (_) {
-                    FocusScope.of(context).requestFocus(_descriptionFocus);
-                  },
-                ),
-                TextFormField(
-                  initialValue: _formData['description']?.toString(),
-                  decoration: const InputDecoration(
-                    labelText: 'Descrição',
-                  ),
-                  keyboardType: TextInputType.multiline,
-                  maxLines: 3,
-                  focusNode: _descriptionFocus,
-                  textInputAction: TextInputAction.next,
-                  onSaved: (description) =>
-                      _formData['description'] = description ?? '',
-                  validator: (_name) {
-                    final name = _name ?? '';
-                    if (name.trim().isEmpty) {
-                      return 'Descrição é obrigatória';
-                    }
-                    if (name.trim().length < 10) {
-                      return 'Descrição precisa no mínimo 10 letras';
-                    }
-                    return null;
-                  },
-                ),
-                Row(crossAxisAlignment: CrossAxisAlignment.end, children: [
-                  Expanded(
-                    child: TextFormField(
-                      controller: _urlController,
-                      decoration: const InputDecoration(
-                        labelText: 'URL da Imagem',
+      body: _isLoading
+          ? Center(
+              child: CircularProgressIndicator(),
+            )
+          : Padding(
+              padding: const EdgeInsets.all(15),
+              child: Form(
+                  key: _formKey,
+                  child: ListView(
+                    children: [
+                      TextFormField(
+                        initialValue: _formData['name']?.toString(),
+                        decoration: const InputDecoration(
+                          labelText: 'Nome',
+                        ),
+                        textInputAction: TextInputAction.next,
+                        onFieldSubmitted: (_) {
+                          FocusScope.of(context).requestFocus(_priceFocus);
+                        },
+                        onSaved: (name) => _formData['name'] = name ?? '',
+                        validator: (_name) {
+                          final name = _name ?? '';
+                          if (name.trim().isEmpty) {
+                            return 'Nome é obrigatório';
+                          }
+                          if (name.trim().length < 3) {
+                            return 'Nome precisa no mínimo 3 letras';
+                          }
+                          return null;
+                        },
                       ),
-                      keyboardType: TextInputType.url,
-                      focusNode: _urlFocus,
-                      textInputAction: TextInputAction.done,
-                      onFieldSubmitted: (_) => _submitForm(),
-                      onSaved: (url) => _formData['url'] = url ?? '',
-                      validator: (_url) {
-                        String url = _url ?? '';
-                        if (!isValidImageUrl(url)) {
-                          return 'Informe uma url válida';
-                          // https://uploads.metropoles.com/wp-content/uploads/2021/06/09110407/cachorro-fofo-usando-oculos_23-2148917262-1-600x380.jpg
-                        }
-                        return null;
-                      },
-                    ),
-                  ),
-                  Container(
-                    height: 100,
-                    width: 100,
-                    margin: const EdgeInsets.only(top: 10, left: 10),
-                    decoration: BoxDecoration(
-                        border: Border.all(color: Colors.grey, width: 1)),
-                    alignment: Alignment.center,
-                    child: _urlController.text.isEmpty
-                        ? const Text('Informe a url')
-                        : Image.network(_urlController.text),
-                  ),
-                ]),
-              ],
-            )),
-      ),
+                      TextFormField(
+                        initialValue: _formData['price']?.toString(),
+                        decoration: const InputDecoration(
+                          labelText: 'Preço',
+                        ),
+                        focusNode: _priceFocus,
+                        keyboardType: const TextInputType.numberWithOptions(
+                            decimal: true),
+                        textInputAction: TextInputAction.next,
+                        onSaved: (price) =>
+                            _formData['price'] = double.parse(price ?? '0'),
+                        validator: (_price) {
+                          final priceString = _price ?? '';
+                          final price = double.tryParse(priceString) ?? -1;
+                          if (price <= 0) {
+                            return 'Informe um preço válido';
+                          }
+                          return null;
+                        },
+                        onFieldSubmitted: (_) {
+                          FocusScope.of(context)
+                              .requestFocus(_descriptionFocus);
+                        },
+                      ),
+                      TextFormField(
+                        initialValue: _formData['description']?.toString(),
+                        decoration: const InputDecoration(
+                          labelText: 'Descrição',
+                        ),
+                        keyboardType: TextInputType.multiline,
+                        maxLines: 3,
+                        focusNode: _descriptionFocus,
+                        textInputAction: TextInputAction.next,
+                        onSaved: (description) =>
+                            _formData['description'] = description ?? '',
+                        validator: (_name) {
+                          final name = _name ?? '';
+                          if (name.trim().isEmpty) {
+                            return 'Descrição é obrigatória';
+                          }
+                          if (name.trim().length < 10) {
+                            return 'Descrição precisa no mínimo 10 letras';
+                          }
+                          return null;
+                        },
+                      ),
+                      Row(
+                          crossAxisAlignment: CrossAxisAlignment.end,
+                          children: [
+                            Expanded(
+                              child: TextFormField(
+                                controller: _urlController,
+                                decoration: const InputDecoration(
+                                  labelText: 'URL da Imagem',
+                                ),
+                                keyboardType: TextInputType.url,
+                                focusNode: _urlFocus,
+                                textInputAction: TextInputAction.done,
+                                onFieldSubmitted: (_) => _submitForm(),
+                                onSaved: (url) => _formData['url'] = url ?? '',
+                                validator: (_url) {
+                                  String url = _url ?? '';
+                                  if (!isValidImageUrl(url)) {
+                                    return 'Informe uma url válida';
+                                    // https://uploads.metropoles.com/wp-content/uploads/2021/06/09110407/cachorro-fofo-usando-oculos_23-2148917262-1-600x380.jpg
+                                  }
+                                  return null;
+                                },
+                              ),
+                            ),
+                            Container(
+                              height: 100,
+                              width: 100,
+                              margin: const EdgeInsets.only(top: 10, left: 10),
+                              decoration: BoxDecoration(
+                                  border:
+                                      Border.all(color: Colors.grey, width: 1)),
+                              alignment: Alignment.center,
+                              child: _urlController.text.isEmpty
+                                  ? const Text('Informe a url')
+                                  : Image.network(_urlController.text),
+                            ),
+                          ]),
+                    ],
+                  )),
+            ),
     );
   }
 }
