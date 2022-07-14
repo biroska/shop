4dca1d61d13f9adf9b4b5c1fcea8bf6d5ab410fe
Aimbere Galdino <biroska@gmail.com>
2022-07-13 22:32:19 -0300

    Usando FutureBuilder para requisicoes assincronas em stateless widget

--- lib/screens/orders_screen.dart
+++ lib/screens/orders_screen.dart
@@ -4,48 +4,36 @@ import 'package:shop/components/app_drawer.dart';
 import 'package:shop/components/order.dart';
 import 'package:shop/models/order_list.dart';
 
-class OrdersScreen extends StatefulWidget {
+class OrdersScreen extends StatelessWidget {
   const OrdersScreen({Key? key}) : super(key: key);
 
   @override
-  State<OrdersScreen> createState() => _OrdersScreenState();
-}
-
-class _OrdersScreenState extends State<OrdersScreen> {
-
-  bool _isLoading = true;
-
-  Future<void> _refreshOrders() {
-    return Provider.of<OrderList>(context, listen: false)
-        .loadOrders()
-        .then((_) => setState(() => _isLoading = false));
-  }
-
-  @override
-  void initState() {
-    super.initState();
-    _refreshOrders();
-  }
-
-  @override
   Widget build(BuildContext context) {
-    final OrderList orders = Provider.of(context);
-
     return Scaffold(
       appBar: AppBar(
         title: const Text('Meus Pedidos'),
       ),
       drawer: const AppDrawer(),
-      body: RefreshIndicator(
-        onRefresh: () => _refreshOrders(),
-        child: _isLoading
-            ? const Center(
-                child: CircularProgressIndicator(),
-              )
-            : ListView.builder(
+      body: FutureBuilder(
+        future: Provider.of<OrderList>(context, listen: false).loadOrders(),
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return const Center(
+              child: CircularProgressIndicator(),
+            );
+          } else if (snapshot.error != null) {
+            return const Center(
+              child: Text('Ocorreu um erro!'),
+            );
+          } else {
+            return Consumer<OrderList>(
+              builder: (ctx, orders, child) => ListView.builder(
                 itemCount: orders.itemsCount,
                 itemBuilder: (ctx, i) => OrderWidget(order: orders.items[i]),
               ),
+            );
+          }
+        },
       ),
     );
   }
